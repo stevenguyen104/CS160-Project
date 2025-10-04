4d37a79bc3b822351e6749369eb4c3b4095cf271
diuekwx <jerisonw@gmail.com>
2025-10-03 21:37:01 -0700

    landing page init

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,43 @@
+import "./LandingPage.css";
+ 
+
+export default function LandingPage(){
+    return (
+        <>
+            <div className="page">
+                <div className= "navbar">
+                    <h3 id="icon">
+                        cool ass icon
+                    </h3>
+
+                    <button id="login-btn">
+                        LOG IN
+                    </button>
+
+                </div>
+                <div className="main">
+                    <div id="title">
+                        <h1>
+                            CARBON COMPASS
+                        </h1> 
+                        <br/>
+                    </div>
+                    <div id="desc">
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+                        </p>
+                    </div>
+                </div>
+
+                <div className="cards">
+                    <div>
+
+                    </div>
+                </div>
+
+
+
+            </div>
+        </>
+    )
+}
