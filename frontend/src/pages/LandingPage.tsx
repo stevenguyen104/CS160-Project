e26331d8f8b627172b22acd1dbd8488d4e976f46
Jerison <jerisonw@gmail.com>
2025-10-11 16:47:07 -0700

    landing page remodel

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,6 +1,5 @@
 import Navbar from "../components/NavBar/Navbar";
 import "./LandingPage.css";
- 
 
 export default function LandingPage(){
     return (
@@ -11,16 +10,10 @@ export default function LandingPage(){
                     <Navbar/>
                 </div>
                 <div className="main">
-                    <div id="title">
+                    <div className="window">
                         <h1>
-                            CARBON COMPASS
-                        </h1> 
-                        <br/>
-                    </div>
-                    <div id="desc">
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-                        </p>
+                            Travel towards a cleaner future.
+                        </h1>
                     </div>
                 </div>
 
