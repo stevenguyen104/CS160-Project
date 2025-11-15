402ca57f62982c2aaaa5d38ea1d65ba52517c744
Royce Li <76494636+rtest42@users.noreply.github.com>
2025-11-15 14:45:24 -0800

    Update alerts.py

--- backend/api/alerts.py
+++ backend/api/alerts.py
@@ -27,6 +27,6 @@ def get_alerts():
     return jsonify({
         "success": True,
         "alerts": displayed_alerts,
-        "message": f"{len(displayed_alerts)} location{"" if len(displayed_alerts) == 1 else "s"}"
+        "message": f"{len(displayed_alerts)} location{'' if len(displayed_alerts) == 1 else 's'}"
                    f" have a moderate AQI or worse."
     }), 200
